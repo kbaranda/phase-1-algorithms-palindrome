afe7b77db7dec4826daf971f451b326f2386ff86
Kat <bolivarkat@yahoo.com>
2021-10-08 15:01:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let a = word.split("")
+  let reverseA = a.reverse()
+  let reverseWord = reverseA.join("")
+  if (word == reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+  take the word that is written in the function
+  break down the word
+  */
+ 
+ /*
+ Add written explanation of your solution here
+ make "word" into an array - word.split("")
+ reverse the array - word.reverse()
+ turn back into a string - word.join("")
+ use if then for true or false
+ */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
